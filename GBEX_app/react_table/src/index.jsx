4c338ded07be1fe5af0af6f05659c40ada8670db
Lasse Ebdrup Pedersen <lasse.ebdrup@gmail.com>
2020-02-25 22:57:04 +0100

    added custom column widths

--- GBEX_app/react_table/src/index.jsx
+++ GBEX_app/react_table/src/index.jsx
@@ -32,6 +32,13 @@ window.column_shows = "col_shows";
 window.table_settings = {};
 window.table_settings[window.whodis] = {};
 window.table_settings[window.whodis][window.column_widths] = Object.assign({}, ...window.columns.map(d => ({[d]: d.length*12+50}))); //default widhts based on name length
+let custom_cws = {
+    "Oligo": {"name": 235, "responsible": 199, "Usage": 349, "OligoType": 158, "Sequence": 614},
+    "Plasmid": {"name": 232, "responsible": 182, "Usage": 212, "GenbankFile": 182},
+    "RawResult": {"name": 148, "responsible": 182, "Description": 259, "ResultType": 170, "DataFile": 299},
+    "Experiment": {"name": 148, "responsible": 182, "Description": 304, "Oligos": 718, "Plasmids": 349, "Results": 134}
+}
+window.table_settings[window.whodis][window.column_widths] = custom_cws[window.whodis]
 window.table_settings[window.whodis][window.column_shows] = [...window.columns]; //default show all
 
 ReactDOM.render(
--- README.md
+++ README.md
@@ -7,11 +7,6 @@ Hosted on mad7.ebdrup.biosustain.dtu.dk
 - Test with figures.ipynb
 - Upload final data 
 #### Bugs/fixes
-- update default column sizes
-    - Oligos 235 199 349 158 614
-    - Plasmids 232 182 212 182
-    - Raw Results 148 182 259 170 299
-    - Experiments 148 182 304 718 349 134
 - Consider need for meta data file on raw results
 - Consider need for raw result type: program output (e.g. hamplicon output)
 #### Objects missing
